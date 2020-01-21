354ccda326769e0d88fe115be0d7ebae615a22ed
Wasim Alayoubi <wasim.alayoubi@mail.concordia.ca>
2020-01-21 11:59:59 -0500

    created maven parent and sub projects

---
+++ Library/src/main/java/ca/concordia/ginacody/comp6231/App.java
@@ -0,0 +1,13 @@
+package ca.concordia.ginacody.comp6231;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
