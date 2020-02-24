7744bd799ee5a6789528eac23bb50f042bc2f28f
Wasim Alayoubi <wasim.alayoubi@mail.concordia.ca>
2020-02-24 00:24:05 -0500

    no more long errors, and shared dems corba package

--- Server/src/main/java/ca/concordia/ginacody/comp6231/services/EventManagementServiceCORBAImpl.java
+++ Server/src/main/java/ca/concordia/ginacody/comp6231/services/EventManagementServiceCORBAImpl.java
@@ -1,6 +1,7 @@
 package ca.concordia.ginacody.comp6231.services;
 
 import ca.concordia.ginacody.comp6231.enums.EventType;
+import ca.concordia.ginacody.comp6231.exception.EventManagementServiceException;
 import ca.concordia.ginacody.comp6231.facade.EventManagementBusinessDelegate;
 import dems.EventManagementServiceImplPOA;
 import org.omg.CORBA.ORB;
@@ -28,47 +29,74 @@ public class EventManagementServiceCORBAImpl extends EventManagementServiceImplP
 
     @Override
     public String login(String userName) {
-        EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
-        return eventManagementBusinessDelegate.login(userName);
+        try {
+            EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
+            return eventManagementBusinessDelegate.login(userName);
+        } catch(EventManagementServiceException e) {
+            return e.getMessage();
+        }
+
 
     }
 
     @Override
     public String addEvent(String eventID, String eventType, int bookingCapacity) {
-        EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
-        return eventManagementBusinessDelegate.addEvent(eventID, EventType.get(eventType), bookingCapacity);
-
+        try {
+            EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
+            return eventManagementBusinessDelegate.addEvent(eventID, EventType.get(eventType), bookingCapacity);
+        } catch(EventManagementServiceException e) {
+            return e.getMessage();
+       }
     }
 
     @Override
     public String removeEvent(String eventID, String eventType) {
-        EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
-        return eventManagementBusinessDelegate.removeEvent(eventID, EventType.get(eventType));
+        try {
+            EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
+            return eventManagementBusinessDelegate.removeEvent(eventID, EventType.get(eventType));
+        } catch(EventManagementServiceException e) {
+            return e.getMessage();
+        }
     }
 
     @Override
     public String listEventAvailability(String eventType) {
-        EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
-        return eventManagementBusinessDelegate.listEventAvailability(EventType.get(eventType));
-
+        try {
+            EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
+            return eventManagementBusinessDelegate.listEventAvailability(EventType.get(eventType));
+        } catch(EventManagementServiceException e) {
+            return e.getMessage();
+        }
     }
 
     @Override
     public String bookEvent(String customerID, String eventID, String eventType) {
-        EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
-        return eventManagementBusinessDelegate.bookEvent(customerID, eventID, EventType.get(eventType));
+        try {
+            EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
+            return eventManagementBusinessDelegate.bookEvent(customerID, eventID, EventType.get(eventType));
+        } catch(EventManagementServiceException e) {
+            return e.getMessage();
+        }
     }
 
     @Override
     public String getBookingSchedule(String customerID) {
-        EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
-        return eventManagementBusinessDelegate.getBookingSchedule(customerID);
+        try {
+            EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
+            return eventManagementBusinessDelegate.getBookingSchedule(customerID);
+        } catch(EventManagementServiceException e) {
+            return e.getMessage();
+        }
     }
 
     @Override
     public String cancelEvent(String customerID, String eventID, String eventType) {
-        EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
-        return eventManagementBusinessDelegate.cancelEvent(customerID, eventID, EventType.get(eventType));
+        try {
+            EventManagementBusinessDelegate eventManagementBusinessDelegate = new EventManagementBusinessDelegate();
+            return eventManagementBusinessDelegate.cancelEvent(customerID, eventID, EventType.get(eventType));
+        } catch(EventManagementServiceException e) {
+            return e.getMessage();
+        }
     }
 
     @Override
